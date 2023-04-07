3e9414aeab123a0213da24aebabcd8b9ca23a724
AbnerMarquezini <contatomarqueziniabner@gmail.com>
2023-04-07 10:11:39 -0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function Calcular(){
     let ladoa = document.getElementById('lado-a')
+    let a = Number(ladoa.value)
     let ladob = document.getElementById('lado-b')
-    let res = document.getElementById('res')
-    let a = ladoa.value
-    let b = ladob.value
-    let c = Math.hypot(a, b)
+    let b = Number(ladob.value)
+    let res = document.getElementById('res')    
+    let c = Number(Math.hypot(a, b).toFixed(2))
 
-    if (a==0 || b==0){
-        res.innerHTML = "Não foi possível calcular"
+    if (a==0 || b==0 || c==0){
+        res.innerHTML = `Não foi possível constituir um triângulo pois não há medida igual a 0cm.`
     } else {
         res.innerHTML = `O valor do lado C é ${c}.`
     }
+}
+
+function Limpar(){
+    document.getElementById('form').reset()
+    let res = document.getElementById('res')
+    res.innerHTML = ''
 }
